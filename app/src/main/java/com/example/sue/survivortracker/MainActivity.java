581fa656adedc8680c55ad455c0073664c70af5a
ansue1234 <ansue1234@gmail.com>
2017-10-14 23:39:08 -0700

    Unable to load

--- app/src/main/java/com/example/sue/survivortracker/MainActivity.java
+++ app/src/main/java/com/example/sue/survivortracker/MainActivity.java
@@ -1,3 +1,323 @@
+//package com.example.sue.survivortracker;
+//import com.google.firebase.database.ChildEventListener;
+//import com.google.firebase.database.DataSnapshot;
+//import com.google.firebase.database.DatabaseError;
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
+//import com.google.firebase.database.Query;
+//import com.google.firebase.database.ValueEventListener;
+//import android.os.Bundle;
+//import android.support.design.widget.FloatingActionButton;
+//import android.support.design.widget.Snackbar;
+//import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.widget.Toolbar;
+//import android.util.Log;
+//import android.view.View;
+//import android.view.Menu;
+//import android.view.MenuItem;
+//import android.widget.AdapterView;
+//import android.widget.ArrayAdapter;
+//import android.widget.Button;
+//import android.widget.EditText;
+//import android.widget.ListView;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        //Person user;
+//        super.onCreate(savedInstanceState);
+//
+//        setContentView(R.layout.activity_main);
+//
+//        // Get ListView object from xml
+//        final ListView listView = (ListView) findViewById(R.id.listView);
+//
+//        // Create a new Adapter
+//        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+//                android.R.layout.simple_list_item_1, android.R.id.text1);
+//
+//        // Assign adapter to ListView
+//        listView.setAdapter(adapter);
+//
+//        // Connect to the Firebase database
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//
+//        // Get a reference to the todoItems child items it the database
+//        final DatabaseReference myRef = database.getReference("todoItems");
+//
+//        // Assign a listener to detect changes to the child items
+//        // of the database reference.
+//        myRef.addChildEventListener(new ChildEventListener(){
+//
+//            // This function is called once for each child that exists
+//            // when the listener is added. Then it is called
+//            // each time a new child is added.
+//            @Override
+//            public void onChildAdded(DataSnapshot dataSnapshot, String previousChildName) {
+//                String value = dataSnapshot.getValue(String.class);
+//                adapter.add(value);
+//            }
+//
+//            // This function is called each time a child item is removed.
+//            public void onChildRemoved(DataSnapshot dataSnapshot){
+//                String value = dataSnapshot.getValue(String.class);
+//                adapter.remove(value);
+//            }
+//
+//            // The following functions are also required in ChildEventListener implementations.
+//            public void onChildChanged(DataSnapshot dataSnapshot, String previousChildName){}
+//            public void onChildMoved(DataSnapshot dataSnapshot, String previousChildName){}
+//
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//                // Failed to read value
+//                Log.w("TAG:", "Failed to read value.", error.toException());
+//            }
+//        });
+//      *****************************************************************************************
+//        // Add items via the Button and EditText at the bottom of the window.
+//        final EditText forename = (EditText) findViewById(R.id.firstName);
+//        final EditText surname = (EditText) findViewById(R.id.lastName);
+//        final EditText text = (EditText) findViewById(R.id.todoText);
+//        final Button button = (Button) findViewById(R.id.addButton);
+//
+//
+//        button.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                // Create a new child with a auto-generated ID.
+//                DatabaseReference childRef = myRef.push();
+//
+//                // Set the child's data to the value passed in from the text box.
+////                String first_name =  forename.getText().toString();
+////                String last_name =  surname.getText().toString();
+////                String position =  text.getText().toString();
+//                //childRef.setValue(new Person(first_name, last_name,position));
+//                childRef.setValue(text.getText().toString());
+//                //Person user = new Person(text.getText().toString())
+//
+//                //forename.setText("");
+//                //surname.setText("");
+//                text.setText("");
+//
+//            }
+//        });
+//   *****************************************************************************************
+//        // Delete items when clicked
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                // look for refrence in database
+//                Query myQuery = myRef.orderByValue().equalTo((String)
+//                        listView.getItemAtPosition(position));
+//                // listen to the database
+//                myQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+//                    @Override
+//                    //get a data snapshot of the database
+//                    public void onDataChange(DataSnapshot dataSnapshot) {
+//                        if (dataSnapshot.hasChildren()) {
+//                            //look for the key of the text and remove it
+//                            DataSnapshot firstChild = dataSnapshot.getChildren().iterator().next();
+//
+//                            // removes item in database
+//                            firstChild.getRef().removeValue();
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void onCancelled(DatabaseError databaseError) {
+//                    }
+//                })
+//                ;}
+//        })
+//        ;}
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+//}
+
+//______________________________________________________________________________
+
+//package com.example.sue.survivortracker;
+//import com.google.firebase.database.ChildEventListener;
+//import com.google.firebase.database.DataSnapshot;
+//import com.google.firebase.database.DatabaseError;
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
+//import com.google.firebase.database.Query;
+//import com.google.firebase.database.ValueEventListener;
+//import android.os.Bundle;
+//import android.support.design.widget.FloatingActionButton;
+//import android.support.design.widget.Snackbar;
+//import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.widget.Toolbar;
+//import android.util.Log;
+//import android.view.View;
+//import android.view.Menu;
+//import android.view.MenuItem;
+//import android.widget.AdapterView;
+//import android.widget.ArrayAdapter;
+//import android.widget.Button;
+//import android.widget.EditText;
+//import android.widget.ListView;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        // Get ListView object from xml
+//        final ListView listView = (ListView) findViewById(R.id.listView);
+//
+//        // Create a new Adapter
+//        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+//                android.R.layout.simple_list_item_1, android.R.id.text1);
+//
+//        // Assign adapter to ListView
+//        listView.setAdapter(adapter);
+//
+//        // Connect to the Firebase database
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//
+//        // Get a reference to the todoItems child items it the database
+//        final DatabaseReference myRef = database.getReference("todoItems");
+//
+//        // Assign a listener to detect changes to the child items
+//        // of the database reference.
+//        //shows the things in list
+//        myRef.addChildEventListener(new ChildEventListener(){
+//
+//            // This function is called once for each child that exists
+//            // when the listener is added. Then it is called
+//            // each time a new child is added.
+//            @Override
+//            public void onChildAdded(DataSnapshot dataSnapshot, String previousChildName) {
+//                String value = dataSnapshot.getValue(String.class);
+//                adapter.add(value);
+//            }
+//
+//            // This function is called each time a child item is removed.
+//            public void onChildRemoved(DataSnapshot dataSnapshot){
+//                String value = dataSnapshot.getValue(String.class);
+//                adapter.remove(value);
+//            }
+//
+//            // The following functions are also required in ChildEventListener implementations.
+//            public void onChildChanged(DataSnapshot dataSnapshot, String previousChildName){}
+//            public void onChildMoved(DataSnapshot dataSnapshot, String previousChildName){}
+//
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//                // Failed to read value
+//                Log.w("TAG:", "Failed to read value.", error.toException());
+//            }
+//        });
+//
+//        // Add items via the Button and EditText at the bottom of the window.
+//        final EditText text = (EditText) findViewById(R.id.loc);
+//        final Button button = (Button) findViewById(R.id.addButton);
+//        final Button search = (Button) findViewById(R.id.search);
+//
+//        // adds the text
+//        button.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//
+//                // Create a new child with a auto-generated ID.
+//                DatabaseReference childRef = myRef.push();
+//
+//                // Set the child's data to the value passed in from the text box.
+//                childRef.setValue(text.getText().toString());
+//
+//                text.setText("");
+//
+//            }
+//        });
+//
+//          THIS is the Place that change layout
+//        ************************************************************************************************
+//        search.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                setContentView(R.layout.search_main);
+//            }
+//        });
+//        ************************************************************************************************
+//
+//        // Delete items when clicked
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//
+//            public void onItemClick(AdapterView<?> parent, View view,
+//                                    int position, long id) {
+//                // look for refrence in database
+//                Query myQuery = myRef.orderByValue().equalTo((String)
+//                        listView.getItemAtPosition(position));
+//                // listen to the database
+//                myQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+//                    @Override
+//                    //get a data snapshot of the database
+//                    public void onDataChange(DataSnapshot dataSnapshot) {
+//                        if (dataSnapshot.hasChildren()) {
+//                            //look for the key of the text and remove it
+//                            DataSnapshot firstChild = dataSnapshot.getChildren().iterator().next();
+//
+//                            // removes item in database
+//                            firstChild.getRef().removeValue();
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void onCancelled(DatabaseError databaseError) {
+//                    }
+//                })
+//                ;}
+//        })
+//        ;}
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+//}
+//________________________________________________________________________________________
+
 package com.example.sue.survivortracker;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -7,8 +327,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+//import android.support.design.widget.FloatingActionButton;
+//import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -23,11 +343,10 @@ import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //Person user;
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
         // Get ListView object from xml
@@ -77,26 +396,17 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // Add items via the Button and EditText at the bottom of the window.
-        final EditText forename = (EditText) findViewById(R.id.firstName);
-        final EditText surname = (EditText) findViewById(R.id.lastName);
-        final EditText text = (EditText) findViewById(R.id.todoText);
+        final EditText text = (EditText) findViewById(R.id.loc);
         final Button button = (Button) findViewById(R.id.addButton);
 
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+
                 // Create a new child with a auto-generated ID.
                 DatabaseReference childRef = myRef.push();
 
                 // Set the child's data to the value passed in from the text box.
-                //String first_name =  forename.getText().toString();
-                //String last_name =  surname.getText().toString();
-                //String position =  text.getText().toString();
-                //childRef.setValue(new Person(first_name, last_name,position));
                 childRef.setValue(text.getText().toString());
-                //Person user = new Person(text.getText().toString())
-
-
-                text.setText("");
 
             }
         });
@@ -104,20 +414,17 @@ public class MainActivity extends AppCompatActivity {
         // Delete items when clicked
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // look for refrence in database
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
                 Query myQuery = myRef.orderByValue().equalTo((String)
                         listView.getItemAtPosition(position));
-                // listen to the database
+
                 myQuery.addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
-                    //get a data snapshot of the database
                     public void onDataChange(DataSnapshot dataSnapshot) {
                         if (dataSnapshot.hasChildren()) {
-                            //look for the key of the text and remove it
                             DataSnapshot firstChild = dataSnapshot.getChildren().iterator().next();
-                            
-                            // removes item in database
                             firstChild.getRef().removeValue();
                         }
                     }
